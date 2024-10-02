a0815e1ddfaf48ef28e402fda2ff2d5b14cc8ebe
Matthintosh <rocher.matthieu@gmail.com>
2024-10-02 17:43:26 +0200

    fix: Logo + text in center

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { PORTFOLIO_WEBSITE } from "./constants";
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen min-w-full items-center p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="flex flex-col min-h-screen min-w-full justify-center items-center p-20 font-[family-name:var(--font-geist-sans)]">
       <Image src={"/images/GeniusCodeCraftLogo.svg"} alt="Logo GeniusCodeCraft" width={300} height={300} />
       <p className="text-2xl sm:text-5xl md:text-8xl bg-gradient-to-br from-[#EF32D9] to-[#00D3CF] inline-block text-transparent bg-clip-text">
         GeniusCodeCraft
